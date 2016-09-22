9e1d6d368e1bfd073928af19f72757e65179d88e
katebeavis <katebeavis84@gmail.com>
2016-09-23 00:32:18 +0100

    tided up test titles

--- spec/bet_position_spec.rb
+++ spec/bet_position_spec.rb
@@ -6,17 +6,17 @@ describe 'BetPosition' do
 
   context 'choosing a position' do
 
-    it "it assigns '< 7' inputs to a number less than 7" do
+    it "it assigns 'less than 7' inputs to a number less than 7" do
       input = "Less than 7"
       expect(bet_position.convert_position_to_number(input)).to eq(1)
     end
 
-    it "assigns '> 7' inputs to a number more than 7" do
+    it "assigns 'greater than 7' inputs to a number more than 7" do
       input = "Greater than 7"
       expect(bet_position.convert_position_to_number(input)).to eq(8)
     end
 
-    it "assigns '== 7' inputs to 7" do
+    it "assigns 'equal' inputs to 7" do
       input = "Equal"
       expect(bet_position.convert_position_to_number(input)).to eq(7)
     end
